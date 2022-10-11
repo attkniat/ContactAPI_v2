c5401d8f865d824d97b6267a7016ff2deb08849a
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2022-10-11 15:42:03 -0300

    Creating the endpoint to the crud
    Setting the Swagger to the prod run also

--- ContactApi_v2/ContactApi_v2/Controllers/ContactController.cs
+++ ContactApi_v2/ContactApi_v2/Controllers/ContactController.cs
@@ -17,6 +17,23 @@ namespace ContactApi_v2.Controllers
         }
 
         [HttpGet]
+        [Route("get-all-contact")]
         public List<Contact> GetAllContact() => _contactRepository.GetContactsAsync().Result;
+
+        [HttpGet]
+        [Route("get-contact-by-id")]
+        public Contact GetContactById(int contactId) => _contactRepository.GetContactByIdAsync(contactId).Result;
+        
+        [HttpPost]
+        [Route("create-contact")]
+        public bool CreateContact(Contact contactToCreate) => _contactRepository.CreateContactAsync(contactToCreate).Result;
+
+        [HttpPut]
+        [Route("update-contact")]
+        public bool UpdateContact(Contact contactToUpdate) => _contactRepository.UpdateContactAsync(contactToUpdate).Result;
+
+        [HttpDelete]
+        [Route("delete-contact")]
+        public bool DeleteContact(int contactId) => _contactRepository.DeletePostAsync(contactId).Result;
     }
 }
