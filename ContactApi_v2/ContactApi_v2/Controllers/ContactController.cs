008df77e861be720131e7544046ce894481410f1
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2022-10-10 22:29:10 -0300

    Controller ajusts

--- ContactApi_v2/ContactApi_v2/Controllers/ContactController.cs
+++ ContactApi_v2/ContactApi_v2/Controllers/ContactController.cs
@@ -1,39 +1,22 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.Extensions.Logging;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
+﻿using ContactEngine.ContactModel;
+using ContactEngine.ContactsInterfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
-//namespace ContactApi_v2.Controllers
-//{
-//    [ApiController]
-//    [Route("[controller]")]
-//    public class ContactController : ControllerBase
-//    {
-//        private static readonly string[] Summaries = new[]
-//        {
-//            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-//        };
+namespace ContactApi_v2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContactController : ControllerBase
+    {
+        private readonly IContactRepository _contactRepository;
 
-//        private readonly ILogger<WeatherForecastController> _logger;
+        public ContactController(IContactRepository contactRepository)
+        {
+            _contactRepository = contactRepository;
+        }
 
-//        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-//        {
-//            _logger = logger;
-//        }
-
-//        [HttpGet]
-//        public IEnumerable<WeatherForecast> Get()
-//        {
-//            var rng = new Random();
-//            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-//            {
-//                Date = DateTime.Now.AddDays(index),
-//                TemperatureC = rng.Next(-20, 55),
-//                Summary = Summaries[rng.Next(Summaries.Length)]
-//            })
-//            .ToArray();
-//        }
-//    }
-//}
+        [HttpGet]
+        public List<Contact> GetAllContact() => _contactRepository.GetContactsAsync().Result;
+    }
+}
