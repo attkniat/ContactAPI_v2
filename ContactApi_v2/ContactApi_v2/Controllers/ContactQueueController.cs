d2f70189f17ab0710ef079eb470efaafc614f46f
Nathan Soares Oliveira <50963930+attkniat@users.noreply.github.com>
2022-10-11 22:12:33 -0300

    Controller Send msg Queue Contacts

--- ContactApi_v2/ContactApi_v2/Controllers/ContactQueueController.cs
+++ ContactApi_v2/ContactApi_v2/Controllers/ContactQueueController.cs
@@ -27,5 +27,69 @@ namespace ContactApi_v2.Controllers
                 throw new Exception($"Send message Error --->  {ex.Message}");
             }
         }
+
+        [HttpPost]
+        [Route("get-contact-get-id-send-queue")]
+        public async Task ContactGetByIdQueue([FromBody] int contactId)
+        {
+            try
+            {
+                var queueCliente = new QueueClient(ConstantsQueue.StorageAccountStr, ConstantsQueue.ContactQueueName);
+                var jsonContact = JsonSerializer.Serialize(contactId);
+                await queueCliente.SendMessageAsync(jsonContact);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Send message Error --->  {ex.Message}");
+            }
+        }
+
+        [HttpPost]
+        [Route("create-contact-send-queue")]
+        public async Task CreateContactQueue([FromBody] Contact contact)
+        {
+            try
+            {
+                var queueCliente = new QueueClient(ConstantsQueue.StorageAccountStr, ConstantsQueue.ContactQueueName);
+                var jsonContact = JsonSerializer.Serialize(contact);
+                await queueCliente.SendMessageAsync(jsonContact);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Send message Error --->  {ex.Message}");
+            }
+        }
+
+        [HttpPost]
+        [Route("update-contact-send-queue")]
+        public async Task UpdateContactQueue([FromBody] Contact contact)
+        {
+            try
+            {
+                var queueCliente = new QueueClient(ConstantsQueue.StorageAccountStr, ConstantsQueue.ContactQueueName);
+                var jsonContact = JsonSerializer.Serialize(contact);
+                await queueCliente.SendMessageAsync(jsonContact);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Send message Error --->  {ex.Message}");
+            }
+        }
+
+        [HttpPost]
+        [Route("delete-contact-send-queue")]
+        public async Task DeleteContactQueue([FromBody] int contactId)
+        {
+            try
+            {
+                var queueCliente = new QueueClient(ConstantsQueue.StorageAccountStr, ConstantsQueue.ContactQueueName);
+                var jsonContact = JsonSerializer.Serialize(contactId);
+                await queueCliente.SendMessageAsync(jsonContact);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Send message Error --->  {ex.Message}");
+            }
+        }
     }
 }
